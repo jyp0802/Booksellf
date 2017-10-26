07f6158b5f41d4de1a71f5060d0c55d28e76d069
Junyong Park <jyp0802@hotmail.com>
2017-10-26 23:39:22 +0900

    first server

---
+++ server0.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('static'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+/* http://127.0.0.1:8081/에 대한 요청 처리 */
+app.get('/', function (req, res) {
+	res.render('index.html')
+});
+
+/* http://127.0.0.8081/up 에 대한 요청 처리 */
+app.get('/up', function(req, res) {
+	res.render('up.html');
+});
+
+/* http://127.0.0.8081/wall 에 대한 요청 처리 */
+app.get('/wall', function(req, res) {
+	res.render('wall_e.html');
+});
+
+/* 서버를 port 8081로 실행 */
+var server = app.listen(8080, function() {
+	console.log("Server running at http://127.0.0.1:8080");
+});
