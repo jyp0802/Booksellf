94f1724e214c433e6f2769bf8c5e83ce502ceace
porkyeye <cmpark0917@kaist.ac.kr>
2017-10-27 01:13:40 +0900

    basic frontend

--- server0.js
+++ server0.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('static'));
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
